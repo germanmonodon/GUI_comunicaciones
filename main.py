0b48ca70222fcea990a3ddf9ebcfc58ca4fddec4
unknown <german@monodon.es>
2023-02-09 15:41:24 +0100

    Pantalla principal y ventana emergente

--- main.py
+++ main.py
@@ -2,26 +2,48 @@ from tkinter import *
 from PIL import ImageTk, Image
 import os
 
+
 class MainWindow:
     def __init__(self):
+
         self.root = Tk()  # create a root widget
         self.root.title("Communication system")
-        #self.root.geometry("1000x1000")
-        self.root.configure(background="white")
-        frame = Frame(self.root, width=1500, height=1000)
-        frame.pack()
+        self.root.geometry("1000x700")
+        # self.root.configure(background="white")
         self.root.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__))+"\Imagenes\monodon_logo.ico")
-        img = ImageTk.PhotoImage(Image.open("Imagenes\monodon_background.png"))
+        # Habría que redimensionar la imagen para que no ocupe tanto, ya sea mediante código o directamente
 
-        # Create a Label Widget to display the text or Image
-        label = Label(frame, image=img)
-        label.pack()
-        self.show_pressure()
+        img = ImageTk.PhotoImage(Image.open("Imagenes\monodon_background.png"))
+        label = Label(self.root, image=img)
+        label.grid(row=0, column=0)
+
+        self.button_enter = Button(text="Realizar conexión entre ROVs", command=self.connect_ROVs)
+        self.button_enter.place(x=400, y=600)
         self.root.mainloop()
 
-    def show_pressure(self):
-        Label(self.root, text="77 Pa").place(x=5, y=0)
+    def connect_ROVs(self):
+        nueva_ventana = SecondWindow()
+class SecondWindow(Toplevel):
+    def __init__(self):
+        super().__init__()
+        self.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__)) + "\Imagenes\monodon_logo.ico")
+        self.title("Interfaz de comunicación")
+        self.boton_cerrar = Button(
+            self,
+            text="Cerrar ventana",
+            command=self.destroy
+        )
+        self.boton_cerrar.place(x=75, y=75)
+        self.focus()
+        self.grab_set()
+
+
+
+
+
+def main():
+    MainWindow()
 
 
 if __name__ == "__main__":
-    MainWindow()
+    main()
