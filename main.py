20c9e17d47ef3c52d48cca3b15880baf60f2aed8
“germanmonodon” <germanmonodon@gmail.es>
2023-02-10 12:10:21 +0100

    Data logger

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+NavigationToolbar2Tk)
 import os
 
 
@@ -20,11 +23,53 @@ class MainWindow:
         label.grid(row=0, column=0)
 
         self.button_enter = Button(text="Realizar conexión entre ROVs", command=self.connect_ROVs)
-        self.button_enter.place(x=400, y=600)
+        self.button_enter.place(x=250, y=600)
+        self.button_data_logger = Button(text="Data Logger", command=self.logger)
+        self.button_data_logger.place(x=550, y=600)
         self.root.mainloop()
 
     def connect_ROVs(self):
         nueva_ventana = SecondWindow(self.root)
+
+    def logger(self):
+        logger_window = ThirdWindow(self.root)
+
+
+class ThirdWindow:
+    def __init__(self, master):
+        self.frame_data = Toplevel(master)
+        self.frame_data.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__)) + "\Imagenes\monodon_logo.ico")
+        self.frame_data.title("Data")
+        self.frame_data.geometry("1000x700")
+        Label(self.frame_data, text="Pressure received").place(x=30, y=10)
+        Label(self.frame_data, text="Timestamp last message : 10/02/2023 12:01").place(x=30, y=400)
+        Label(self.frame_data, text="Pressure received : 1002 Pa").place(x=30, y=420)
+        Label(self.frame_data, text="Estimated depth: 2 meters").place(x=30, y=440)
+        self.plot_pressure()
+
+    def plot_pressure(self):
+        fig = Figure(figsize=(5, 5),
+                     dpi=70)
+
+        # list of squares
+        y = [i ** 2 for i in range(101)]
+
+        # adding the subplot
+        plot1 = fig.add_subplot(111)
+
+        # plotting the graph
+        plot1.plot(y)
+        plot1.set_xlabel('Time (s)')
+        plot1.set_ylabel('Pressure (Pa)')
+
+        # creating the Tkinter canvas
+        # containing the Matplotlib figure
+        canvas = FigureCanvasTkAgg(fig,
+                                   master=self.frame_data)
+        canvas.draw()
+
+        # placing the canvas on the Tkinter window
+        canvas.get_tk_widget().place(x=40, y=40)
 
 
 class SecondWindow:
@@ -32,7 +77,7 @@ class SecondWindow:
         self.frameROVs = Toplevel(master)
         self.frameROVs.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__)) + "\Imagenes\monodon_logo.ico")
         self.frameROVs.title("Interfaz de comunicación")
-        self.frameROVs.geometry("1000x700")
+        self.frameROVs.geometry("1000x300")
         self.frameROVs.grid_columnconfigure(0, weight=1)
         self.frameROVs.grid_columnconfigure(1, weight=1)
         self.frameROVs.grid_rowconfigure(0, weight=1)
