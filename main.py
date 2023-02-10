1c1ee356e26761e7fad104f9f67d6c445ad6027a
“germanmonodon” <germanmonodon@gmail.es>
2023-02-10 10:23:39 +0100

    Requirements and new window customized

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk, Image
 import os
 
@@ -11,6 +12,7 @@ class MainWindow:
         self.root.geometry("1000x700")
         # self.root.configure(background="white")
         self.root.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__))+"\Imagenes\monodon_logo.ico")
+
         # Habría que redimensionar la imagen para que no ocupe tanto, ya sea mediante código o directamente
 
         img = ImageTk.PhotoImage(Image.open("Imagenes\monodon_background.png"))
@@ -31,21 +33,24 @@ class SecondWindow:
         self.frameROVs.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__)) + "\Imagenes\monodon_logo.ico")
         self.frameROVs.title("Interfaz de comunicación")
         self.frameROVs.geometry("1000x700")
-        Label(self.frameROVs,
+        self.frameROVs.grid_columnconfigure(0, weight=1)
+        self.frameROVs.grid_columnconfigure(1, weight=1)
+        self.frameROVs.grid_rowconfigure(0, weight=1)
+
+        master_rov = Frame(self.frameROVs, bg="#38E7E5")
+        master_rov.grid(row=0, column=0, sticky="nesw")
+        slave_rov = Frame(self.frameROVs, bg="#3FF6A1")
+        slave_rov.grid(row=0, column=1, sticky="nesw")
+        Label(master_rov,
               text="Master ROV").place(x=250, y=50)
-        Label(self.frameROVs,
-              text="Slave ROV").place(x=750, y=50)
-        #self.boton_cerrar = Button(
-        #    self,
-         #   text="Cerrar ventana",
-          #  command=self.destroy
-        #)
-        #self.boton_cerrar.place(x=75, y=75)
-        #self.focus()
-        # self.grab_set()
-
-
-
+        Label(slave_rov,
+              text="Slave ROV").place(x=250, y=50)
+        Label(master_rov, text="Enviando mensaje..").place(x=250, y=100)
+        progressbar = ttk.Progressbar(self.frameROVs)
+        progressbar.place(x=400, y=100, width=200)
+        Label(master_rov, text="Recibiendo mensaje..").place(x=250, y=200)
+        progressbar = ttk.Progressbar(self.frameROVs)
+        progressbar.place(x=400, y=200, width=200)
 
 
 def main():
