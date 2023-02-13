ba22c9dced3201803a1de3afd8c1d049653779c0
unknown <german@monodon.es>
2023-02-13 11:40:00 +0100

    Nuevos ajustes y modificacion ThirdWindow class

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PIL import ImageTk, Image
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 NavigationToolbar2Tk)
+from tkdial import Meter
+import numpy as np
 import os
 
 
@@ -40,12 +42,58 @@ class ThirdWindow:
         self.frame_data = Toplevel(master)
         self.frame_data.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__)) + "\Imagenes\monodon_logo.ico")
         self.frame_data.title("Data")
-        self.frame_data.geometry("1000x700")
+        self.frame_data.geometry("1700x700")
         Label(self.frame_data, text="Pressure received").place(x=30, y=10)
+        Label(self.frame_data, text="Accelerometer data").place(x=900, y=10)
+        Label(self.frame_data, text="Number of samples: 80").place(x=900, y=400)
+        Label(self.frame_data, text="Orientation ROV2").place(x=500, y=10)
+        Label(self.frame_data, text="Timestamp last message : 13/02/2023 10:34").place(x=500, y=400)
+        Label(self.frame_data, text="Acc data: ").place(x=500, y=420)
+        Label(self.frame_data, text="Gyro data:").place(x=500, y=440)
         Label(self.frame_data, text="Timestamp last message : 10/02/2023 12:01").place(x=30, y=400)
         Label(self.frame_data, text="Pressure received : 1002 Pa").place(x=30, y=420)
         Label(self.frame_data, text="Estimated depth: 2 meters").place(x=30, y=440)
         self.plot_pressure()
+        self.plot_imu()
+        self.show_acc()
+
+    def show_acc(self):
+        fig = Figure(figsize=(5, 5),
+                     dpi=70)
+
+        # random data
+
+        x_acc = np.random.randint(1,101,80)
+        y_acc = np.random.randint(1, 101, 80)
+        z_acc = np.random.randint(1, 101, 80)
+
+        # adding the subplot
+        plot1 = fig.add_subplot(111)
+
+        # plotting the graph
+        plot1.plot(x_acc, color="blue")
+        plot1.plot(y_acc, color="red")
+        plot1.plot(z_acc, color="yellow")
+        plot1.set_xlabel('Time (s)')
+        plot1.set_ylabel('Acceleration (g)')
+
+        # creating the Tkinter canvas
+        # containing the Matplotlib figure
+        canvas = FigureCanvasTkAgg(fig,
+                                   master=self.frame_data)
+        canvas.draw()
+
+        # placing the canvas on the Tkinter window
+        canvas.get_tk_widget().place(x=900, y=40)
+
+    def plot_imu(self):
+
+        meter3 = Meter(self.frame_data, bg="#242424", fg="#242424", radius=320, start=0, end=360,
+                       major_divisions=90, border_width=0, text_color="white",
+                       start_angle=0, end_angle=-360, scale_color="white", axis_color="cyan",
+                       needle_color="white", scroll_steps=60, minor_divisions=10, scroll=False)
+        meter3.set(15)
+        meter3.place(x=500, y=60)
 
     def plot_pressure(self):
         fig = Figure(figsize=(5, 5),
