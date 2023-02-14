019d7c88c3d516f182dc4f8263d57609aa2b7dd3
unknown <german@monodon.es>
2023-02-14 12:31:18 +0100

    Refactor

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
-NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkdial import Meter
 from fpdf import FPDF
-import numpy as np
 from PIL import ImageGrab
+
 import time
 import threading
+import numpy as np
 import os
 import datetime
-
 
 
 class MainWindow:
@@ -21,23 +20,25 @@ class MainWindow:
         self.root = Tk()  # create a root widget
         self.root.title("Communication system")
         self.root.geometry("1000x700")
-        # self.root.configure(background="white")
-        self.root.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__))+"\Imagenes\monodon_logo.ico")
+        self.root.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__))
+                                +"\Imagenes\monodon_logo.ico")
 
-        # Habría que redimensionar la imagen para que no ocupe tanto, ya sea mediante código o directamente
-
-        img = ImageTk.PhotoImage(Image.open(os.path.dirname(os.path.abspath(__file__))+"\Imagenes\monodon_background.png"))
+        img = ImageTk.PhotoImage(Image.open(os.path.dirname(os.path.abspath(__file__))
+                                            + "\Imagenes\monodon_background.png"))
         label = Label(self.root, image=img)
         label.grid(row=0, column=0)
 
-        self.button_enter = Button(text="Realizar conexión entre ROVs", command=self.connect_ROVs)
+        self.button_enter = Button(text="Realizar conexión entre ROVs", command=self.connect_rovs)
         self.button_enter.place(x=250, y=600)
         self.button_data_logger = Button(text="Data Logger", command=self.logger)
         self.button_data_logger.place(x=550, y=600)
+        self.logger_window = None
+        self.image_pdf = None
+        self.button_pdf_download = None
         self.root.mainloop()
 
-    def connect_ROVs(self):
-        nueva_ventana = SecondWindow(self.root)
+    def connect_rovs(self):
+        SecondWindow(self.root)
 
     def logger(self):
         self.logger_window = ThirdWindow(self.root)
@@ -46,6 +47,7 @@ class MainWindow:
 
     def button_pdf(self):
         self.logger_window.button_pdf_download()
+
 
 class Table:
     def __init__(self, root):
@@ -66,16 +68,19 @@ class Table:
 
 class ThirdWindow:
     def __init__(self, master):
+        # Declarations
         self.angle = 0
         self.begin = True
+        self.pdf_name = "NA"
+        self.pdf_name_2 = "NA"
+
+        # Principal Frame configuration
         self.frame_data = Toplevel(master)
         self.frame_data.wm_iconbitmap(os.path.dirname(os.path.abspath(__file__)) + "\Imagenes\monodon_logo.ico")
         self.frame_data.title("Data")
         self.frame_data.geometry("1300x600")
-        self.pdf_name = "NA"
-        self.text_in_label = StringVar()
-        self.entry_text = StringVar()
-        self.text_in_label.set("Enter pdf name to save results and stop running")
+
+        # Labels configuration
         Label(self.frame_data, text="Pressure received").place(x=30, y=10)
         Label(self.frame_data, text="Accelerometer data").place(x=900, y=10)
         Label(self.frame_data, text="Number of samples: 80").place(x=900, y=400)
@@ -86,12 +91,17 @@ class ThirdWindow:
         Label(self.frame_data, text="Timestamp last message : 10/02/2023 12:01").place(x=30, y=400)
         Label(self.frame_data, text="Pressure received : 1002 Pa").place(x=30, y=420)
         Label(self.frame_data, text="Estimated depth: 2 meters").place(x=30, y=440)
+
         self.frame_table = LabelFrame(self.frame_data, text="Coms statistics")
         self.frame_table.place(x=550, y=470)
         self.coms_table = Table(self.frame_table)
+
         self.image_pdf = ImageTk.PhotoImage(Image.open(r"C:\Users\Innovacion\Desktop\Proyectos_Coding\GUI_comunicaciones\Imagenes\pdf_download.jpg"))
-        self.button_pdf_download = Button(self.frame_data, height=70, width=200, command=self.button_pdf_download, image=self.image_pdf)
-        self.button_pdf_download.place(x=40, y=500)
+        self.button_pdf = Button(self.frame_data, height=70, width=200, command=self.button_pdf_download,
+                                 image=self.image_pdf)
+        self.button_pdf.place(x=40, y=500)
+
+        # Threading for interactive GUI
         self.x = threading.Thread(target=self.show_acc)
         self.x.start()
         self.y = threading.Thread(target=self.plot_imu)
@@ -108,35 +118,29 @@ class ThirdWindow:
 
     def button_pdf_download(self):
         pdf = FPDF()
-        # Add a page
         pdf.add_page()
-        # Falta buscar en el direectorio los nombres que tienes y yo lo que haría sería darle la fecha del experimento y al carajo
-        # set style and size of font
-        # that you want in the pdf
+
+        # Giving name to the pdf
         self.pdf_name = str(datetime.datetime.now())
         self.pdf_name = self.pdf_name.split(".")[0]
         self.pdf_name_2 = self.pdf_name.replace(":", "_")
-
-        # ts store timestamp of current time
-
         pdf.set_font("Arial", size=15)
-        pdf.image(r"C:\Users\Innovacion\Desktop\Proyectos_Coding\GUI_comunicaciones\Imagenes\header_monodon.jpg", x=5, y=5, w=10, h=10)
+        pdf.image(r"C:\Users\Innovacion\Desktop\Proyectos_Coding\GUI_comunicaciones\Imagenes\header_monodon.jpg", x=5,
+                  y=5, w=10, h=10)
         # create a cell
         pdf.cell(200, 10, txt="Report Data for experiment done at " + self.pdf_name,
                  ln=1, align='C')
 
-        #self.getter(self.canvas_acc.get_tk_widget())
         pdf.text(x=20, y=40, txt="ROV acceleration")
         pdf.image(r"C:\Users\Innovacion\Desktop\Proyectos_Coding\GUI_comunicaciones\acc.png",
                   x=30, y=40, w=100, h=100)
+
         pdf.text(x=20, y=150, txt="Historic of pressure")
         pdf.image(r"C:\Users\Innovacion\Desktop\Proyectos_Coding\GUI_comunicaciones\pressure.png",
                   x=30, y=160, w=100, h=100)
-        if self.pdf_name == "":
-            self.text_in_label.set("Set a name for the pdf")
-        else:
-            self.text_in_label.set("PDF saved correctly")
-            pdf.output(os.path.dirname(os.path.abspath(__file__)) + "\Informes\Monodon_" + self.pdf_name_2 + ".pdf")
+        #Añadir la tabla
+
+        pdf.output(os.path.dirname(os.path.abspath(__file__)) + "\Informes\Monodon_" + self.pdf_name_2 + ".pdf")
 
     def show_acc(self):
         while True:
@@ -144,7 +148,6 @@ class ThirdWindow:
                          dpi=70)
 
             # random data
-
             x_acc = np.random.randint(1,101,80)
             y_acc = np.random.randint(1, 101, 80)
             z_acc = np.random.randint(1, 101, 80)
@@ -161,8 +164,7 @@ class ThirdWindow:
 
             # creating the Tkinter canvas
             # containing the Matplotlib figure
-            self.canvas_acc = FigureCanvasTkAgg(fig,
-                                       master=self.frame_data)
+            self.canvas_acc = FigureCanvasTkAgg(fig, master=self.frame_data)
             self.canvas_acc.draw()
 
             # placing the canvas on the Tkinter window
